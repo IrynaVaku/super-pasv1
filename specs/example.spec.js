1b830ea3d58b31d15e73a51a2cbab441022858d5
IrynaVaku <vakulovskaya@gmail.com>
2022-02-16 23:38:21 -0800

    add first tests add mocha config

---
+++ specs/example.spec.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+
+describe("Math functions", () => {
+
+      const a = 4
+        const b = 5
+        const c = 9
+        const d = -1
+
+    it("a+b=c", () => {
+        expect(a+b).to.eq(c)
+    })
+    it("a-b=d", () => {
+        expect(a-b).to.eq(d)
+    })
+})
+
+
